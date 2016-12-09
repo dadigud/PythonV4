480d647de9e29228d59b851a52ac69bf42310610
dadi15 <dadi15@ru.is>
2016-12-09 02:30:42 +0000

    EVERYTHING WORKS!!

--- move_files.py
+++ move_files.py
@@ -1,7 +1,7 @@
 import os
 import guessit
 import shutil
-
+import time
 
 def move_me(abp, fon, fn):
     if not os.path.exists(fon):
@@ -9,16 +9,35 @@ def move_me(abp, fon, fn):
     shutil.move(abp, fon + '/' + fn)
 
 
+def MoveFolder1(dir):
+    Exists = []
+    seen = set()
+    for File in os.listdir(dir):
+            if not os.path.isdir(os.path.join(dir, File)):
+                Exists.append(File)
+    for root, dirs, files in os.walk(dir):
+        for file in files:
+            if file not in seen and file not in Exists:
+                seen.add(file)
+                shutil.move(os.path.join(root,file),os.path.join(dir,file))
+    for Dir in os.listdir(dir):
+            if os.path.isdir(os.path.join(dir, Dir)):
+                shutil.rmtree(os.path.join(dir, Dir))
+
+
 def sort_files(root_path):
+    now = time.localtime()
+    MoveFolder1(root_path)
 
     file_extensions = ['.avi', '.mp4', '.mkv', '.m4v']  # Allowed video file extensions
-    banned_extensions = ['.srt', '.jpg', '.torrent']    # Banned file extensions
+    banned_extensions = ['.srt', '.jpg', '.torrent', '.gif']    # Banned file extensions
 
     ef = root_path + '/' + 'Episodes'   # Path to the Episodes folder
     mf = root_path + '/' + 'Movies'     # Path to the Movies folder
     sf = root_path + '/' + 'Songs'      # Path to the Songs folder
     df = root_path + '/' + 'Documents'  # Path to the Documents folder
     pf = root_path + '/' + 'Programs'   # Path to the Programs folder
+    rf = root_path + '/' + 'Random'     # Path to the Random folder
 
     title = ''                          # Title of the file
     filetype = ''                       # Filetype
@@ -28,8 +47,11 @@ def sort_files(root_path):
         abs_path = root_path + '/' + filename
         if not os.path.isdir(abs_path):
             if os.path.splitext(abs_path)[-1].lower() in file_extensions:
+                counter = 0
                 movie_info = guessit.guessit(filename)
+                dict_len = len(movie_info)
                 for key, val in movie_info.items():
+                    counter += 1
                     if key == 'title':
                         title = str(val)
                     if key == 'type':
@@ -46,9 +68,7 @@ def sort_files(root_path):
                             shutil.move(abs_path, ef + '/' + title.title() + '/' + 'Season ' + season)
                         else:
                             shutil.move(abs_path, ef + '/' + title.title() + '/' + 'Season ' + season)
-                        title = ''
-                        filetype = ''
-                        season = ''
+
                     elif title != '' and filetype == 'movie':
                         if not os.path.exists(mf):
                             os.makedirs(mf)
@@ -57,9 +77,16 @@ def sort_files(root_path):
                             shutil.move(abs_path, mf + '/' + title.title())
                         else:
                             shutil.move(abs_path, mf + '/' + title.title())
-                        title = ''
-                        filetype = ''
-                        season = ''
+
+                    elif dict_len == counter and season == '':
+                        if not os.path.exists(rf):
+                            os.makedirs(rf)
+                        shutil.move(abs_path, rf + '/' + filename)
+
+                    title = ''
+                    filetype = ''
+                    season = ''
+
             elif os.path.splitext(abs_path)[-1].lower() in banned_extensions:
                 os.remove(abs_path)
             elif os.path.splitext(abs_path)[-1].lower() == '.mp3':
@@ -68,4 +95,6 @@ def sort_files(root_path):
                 move_me(abs_path, pf, filename)
             elif os.path.splitext(abs_path)[-1].lower() == '.txt':
                 move_me(abs_path, df, filename)
+    print(now, time.localtime())
 
+sort_files('C:/ACCESS/downloads')
